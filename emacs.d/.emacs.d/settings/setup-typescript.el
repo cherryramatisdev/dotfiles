7fe6e14d2dec2d4ac6a409744b73bb3f23c26ea6
Cherry Ramatis <cherry.ramatis@gmail.com>
2021-06-26 15:24:54 -0300

    configure npm mode(this helps me a lot)

--- emacs.d/.emacs.d/settings/setup-typescript.el
+++ emacs.d/.emacs.d/settings/setup-typescript.el
@@ -2,9 +2,12 @@
 (require 'company)
 (require 'prettier-js)
 (require 'zencoding-mode)
+(require 'npm-mode)
 
 (add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
 
+(add-hook 'web-mode-hook 'npm-mode)
+(add-hook 'typescript-mode-hook 'npm-mode)
 (add-hook 'web-mode-hook 'prettier-js-mode)
 (add-hook 'typescript-mode-hook 'prettier-js-mode)
 
