53b61c118009d20df0be4c0ec5c4ca9eb2a77f91
Cherry Ramatis <cherry.ramatis@gmail.com>
2021-06-26 15:23:59 -0300

    change theme to modus from protesilaos

--- emacs.d/.emacs.d/settings/appearance.el
+++ emacs.d/.emacs.d/settings/appearance.el
@@ -1,3 +1,4 @@
+(require 'modus-themes)
 (setq font-lock-maximum-decoration t
       truncate-partial-width-windows nil)
 
@@ -13,14 +14,16 @@
 (setq css-fontify-colors nil)
 
 ;; Set custom theme path
-(setq custom-theme-directory (concat user-emacs-directory "themes"))
+;; (setq custom-theme-directory (concat user-emacs-directory "themes"))
 
-(dolist
-    (path (directory-files custom-theme-directory t "\\w+"))
-  (when (file-directory-p path)
-    (add-to-list 'custom-theme-load-path path)))
+;; (dolist
+;;     (path (directory-files custom-theme-directory t "\\w+"))
+;;   (when (file-directory-p path)
+;;     (add-to-list 'custom-theme-load-path path)))
 
-(load-theme 'default-black t)
+;; (load-theme 'default-black t)
+
+(load-theme 'modus-vivendi t)
 
 ;; Don't defer screen updates when performing operations
 (setq redisplay-dont-pause t)
