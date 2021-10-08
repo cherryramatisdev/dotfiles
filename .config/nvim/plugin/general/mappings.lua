30820079ac9fa6862f7402653d696ff4efb97089
CherryRamatis <cherry.ramatis@lamimed.it>
2021-10-08 13:40:16 -0300

    use <C-o> only on insert and terminal mode, I accidentaly overwrite <C-o> native functionality

--- .config/nvim/plugin/general/mappings.lua
+++ .config/nvim/plugin/general/mappings.lua
@@ -8,10 +8,6 @@ vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true })
 vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true })
 
--- Use <C-o> to exit insert mode in pretty much everything
-vim.api.nvim_set_keymap("n", "<C-o>", "<Esc>", { noremap = true })
-vim.api.nvim_set_keymap("v", "<C-o>", "<Esc>gV", { noremap = true })
-vim.api.nvim_set_keymap("o", "<C-o>", "<Esc>", { noremap = true })
-vim.api.nvim_set_keymap("c", "<C-o>", "<C-c><Esc>", { noremap = true })
+-- Use <C-o> to exit insert mode in terminal and insert mode
 vim.api.nvim_set_keymap("i", "<C-o>", "<Esc>`^", { noremap = true })
 vim.api.nvim_set_keymap("t", "<C-o>", [[<C-\><C-n>]], { noremap = true })
