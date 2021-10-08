7186d5e915bd20e315e315b2f3eeaaec585874d4
CherryRamatis <cherry.ramatis@lamimed.it>
2021-10-08 09:21:35 -0300

    use vertical terminal

--- .config/nvim/lua/plugin/terminal.lua
+++ .config/nvim/lua/plugin/terminal.lua
@@ -5,14 +5,20 @@ if not ok then
 end
 
 t.setup {
-  size = 20,
+  size = function(term)
+    if term.direction == "horizontal" then
+      return 15
+    elseif term.direction == "vertical" then
+      return vim.o.columns * 0.4
+    end
+  end,
   open_mapping = [[<c-t>]],
   hide_numbers = true,
   shade_terminals = true,
   start_in_insert = true,
   insert_mappings = true,
   persist_size = true,
-  direction = "horizontal",
+  direction = "vertical",
   close_on_exit = true,
 }
 
