571892bce40c7ab6286cd84b5f5bc44e151ec8b0
CherryRamatis <cherry.ramatis@lamimed.it>
2021-10-13 17:03:53 -0300

    fish configuratn with emacs

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,12 +1,15 @@
 function fish_user_key_bindings
-  fish_vi_key_bindings
+  # fish_vi_key_bindings
+  fish_default_key_bindings
 
-  bind -M insert \cn 'nvim'
-  bind -M insert \cg 'lazygit'
-  bind -M insert \ck 'up-or-search'
-  bind -M insert \ce 'accept-autosuggestion'
-  bind -M insert \cx\cf 'find-files'
-  bind -M insert \cx\ct 'tmux new-session -A -s main'
-  bind -M insert \cx\cp 'tmux-sessionizer'
-  bind -M insert \cx\ce 'ranger'
+  if test "$fish_key_bindings" = "fish_vi_key_bindings"
+    bind -M insert \cn 'nvim'
+    bind -M insert \cg 'lazygit'
+    bind -M insert \ck 'up-or-search'
+    bind -M insert \ce 'accept-autosuggestion'
+    bind -M insert \cx\cf 'find-files'
+    bind -M insert \cx\ct 'tmux new-session -A -s main'
+    bind -M insert \cx\cp 'tmux-sessionizer'
+    bind -M insert \cx\ce 'ranger'
+  end
 end
