d3653c16e7877a98ccedfebd274aa0145425d46c
CherryRamatis <cherry.ramatis@lamimed.it>
2021-10-26 10:53:36 -0300

    enable cmp on commit message

--- .config/nvim/ftplugin/NeogitCommitMessage.lua
+++ .config/nvim/ftplugin/NeogitCommitMessage.lua
@@ -1,1 +1,20 @@
 vim.opt.spell = true
+vim.opt.spelllang = { "en_us" }
+
+local cmp = require "cmp"
+
+cmp.setup.buffer {
+  sources = {
+    { name = "spell" },
+    { name = "snippy" },
+    {
+      name = "buffer",
+      opts = {
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end,
+      },
+    },
+    { name = "path" },
+  },
+}
