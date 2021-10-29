e12f5870f88ac0c8a5dfbd643dbfbf505d32f453
CherryRamatis <cherry.ramatis@lamimed.it>
2021-10-29 18:29:55 -0300

    remove stuff that i don't use

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -7,8 +7,6 @@ function fish_user_key_bindings
     bind -M insert \cg 'lazygit'
     bind -M insert \ck 'up-or-search'
     bind -M insert \ce 'accept-autosuggestion'
-    bind -M insert \cp 'find-files'
     bind -M insert \ct 'tmux new-session -A -s main'
-    bind -M insert \cf 'tmux-sessionizer'
   end
 end
