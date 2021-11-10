ada80a175513bc71a4ba6bdf0a1686f4166c7151
CherryRamatis <cherry.ramatis@lamimed.it>
2021-11-09 21:08:42 -0300

    emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,1 +1,47 @@
-(org-babel-load-file (expand-file-name (concat user-emacs-directory "/Emacs.org")))
+(package-initialize)
+
+(setq gtls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+(setq package-archives '(("melpa" . "http://melpa.org/packages/")
+			 ("gnu" . "http://elpa.gnu.org/packages/")
+			 )
+      gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
+;; Add ELPA packages to the loadpp path
+(let ((default-directory "~/.emacs.d/elpa"))
+  (normal-top-level-add-subdirs-to-load-path))
+
+;; Ensure use-package is installed and loaded
+(condition-case nil
+    (require 'use-package)
+  (file-error
+   (require 'package)
+   (package-initialize)
+   (package-refresh-contents)
+   (package-install 'use-package)
+   (require 'use-package)))
+
+;; Set to true to have use-package install all packages mentioted if
+;; they're not already installed.
+(setq use-package-always-ensure nil)
+
+;; Minibuffer
+
+;; Enhanced M-x
+(use-package counsel
+  :ensure t
+  :bind
+  (("M-x" . counsel-M-x)))
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(counsel use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
