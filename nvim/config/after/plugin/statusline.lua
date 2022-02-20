bc246e394cd23ddc08130c3de753da183788164a
CherryRamatis <cherry.ramatis@lamimed.it>
2022-02-20 00:45:52 -0300

    Add current branch name to statusline

--- nvim/config/after/plugin/statusline.lua
+++ nvim/config/after/plugin/statusline.lua
@@ -2,11 +2,28 @@ local generator = function()
   local el_segments = {}
 
   local extensions = require("el.extensions")
+  local subscribe = require("el.subscribe")
   table.insert(el_segments, extensions.mode)
 
   table.insert(el_segments, " ")
 
   table.insert(el_segments, "%f")
+
+  table.insert(el_segments, " ")
+
+  table.insert(
+    el_segments,
+    subscribe.buf_autocmd(
+      "el_git_branch",
+      "BufEnter",
+      function(window, buffer)
+        local branch = extensions.git_branch(window, buffer)
+        if branch then
+          return string.format("[%s]", branch)
+        end
+      end
+    )
+  )
 
   table.insert(el_segments, "%=")
 
