ac8d8fe20e2bc75e9f69b3357d63acff45b395b0
CherryRamatis <cherry.ramatis@lamimed.it>
2022-02-20 00:45:28 -0300

    Create a workflow through projects using tabs

--- nvim/config/lua/cherry/telescope/mappings.lua
+++ nvim/config/lua/cherry/telescope/mappings.lua
@@ -1,3 +1,5 @@
+local actions = require "telescope.actions"
+
 vim.keymap.set(
   "n",
   "<leader>f",
@@ -14,10 +16,85 @@ vim.keymap.set(
   end
 )
 
+local function buffers()
+  local opts_with_preview
+
+  opts_with_preview = {
+    prompt_title = "~ buffers ~",
+    attach_mappings = function(_, map)
+      map(
+        "i",
+        "<C-d>",
+        function(prompt_bufnr)
+          local picker = require("telescope.actions.state").get_current_picker(prompt_bufnr)
+          picker:delete_selection(
+            function(selection)
+              vim.api.nvim_buf_delete(selection.bufnr, {force = true})
+            end
+          )
+        end
+      )
+
+      return true
+    end
+  }
+
+  require("telescope.builtin").buffers(opts_with_preview)
+end
+
+vim.keymap.set(
+  "n",
+  "<leader>b",
+  function()
+    buffers()
+  end
+)
+
 vim.keymap.set(
   "n",
   "<leader>e",
   function()
     require("telescope").extensions.file_browser.file_browser()
+  end
+)
+
+local function switchProjects()
+  local opts_with_preview
+
+  opts_with_preview = {
+    prompt_title = "~ projects ~",
+    shorten_path = false,
+    cwd = "~/git/",
+    attach_mappings = function(_, map)
+      map(
+        "i",
+        "<CR>",
+        function(prompt_bufnr)
+          actions.close(prompt_bufnr)
+          local filepath = require("telescope.actions.state").get_selected_entry(prompt_bufnr)[1]
+          local runner = "tabnew | tcd " .. filepath
+
+          if string.len(vim.fn.expand("%")) <= 0 then
+            runner = "tcd " .. filepath
+          end
+
+          vim.cmd(runner)
+
+          require("telescope.builtin").find_files()
+        end
+      )
+
+      return true
+    end
+  }
+
+  require("telescope").extensions.file_browser.file_browser(opts_with_preview)
+end
+
+vim.keymap.set(
+  "n",
+  "<leader>p",
+  function()
+    switchProjects()
   end
 )
--- nvim/config/plugin/mappings.lua
+++ nvim/config/plugin/mappings.lua
@@ -7,3 +7,33 @@ vim.keymap.set(
     vim.cmd(sourcelua)
   end
 )
+
+vim.keymap.set("n", "<Tab>", "gt")
+vim.keymap.set("n", "<S-Tab>", "gT")
+vim.keymap.set("n", "te", ":tabnew<cr>")
+vim.keymap.set("n", "gt", ":FloatermToggle<cr>")
+
+vim.keymap.set("t", "<C-[>", [[<C-\><C-n>]])
+vim.keymap.set("t", "<C-d>", [[<C-\><C-n>:bd!<cr>]])
+
+vim.keymap.set(
+  "n",
+  "<leader><CR>",
+  function()
+    local cmd = vim.fn.input("Run: ")
+
+    -- TODO: improve this
+    if cmd == "back" then
+      vim.cmd [[ sp | terminal yarn start:dev ]]
+      return
+    end
+
+    -- TODO: improve this
+    if cmd == "front" then
+      vim.cmd [[ sp | terminal yarn dev ]]
+      return
+    end
+
+    vim.cmd("sp | terminal " .. cmd)
+  end
+)
